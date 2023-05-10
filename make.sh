53148c3ea2a3bb6958c20712b10ecef58fe9c2ec
Ryan Downey <ryandowney059@Ryans-Laptop>
2023-05-09 23:23:22 -0400

    working

---
+++ make.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Get Submodules
+git clone https://github.com/engineerOfLies/gfc.git
+cd gfc
+git clone https://github.com/engineerOfLies/simple_json.git
+git clone https://github.com/engineerOfLies/simple_logger.git
+cd ./simple_json/src
+
+# Install Submodules
+make
+make static
+cd ../..
+# pwd
+cd ./simple_logger/src
+make
+make static
+cd ../..
+# pwd
+cd ./src
+make
+make static
+
+# Build total project and run
+cd ../..
+# pwd
+cd ./src
+make
+echo "compiled"
+
+# Run Project
+cd ..
+./gf2d
